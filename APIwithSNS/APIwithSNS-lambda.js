b4a285ad65473fccbc9b33a70d0f2f08ab31d410
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-11 14:55:18 +0530

    SNS added

--- APIwithSNS/APIwithSNS-lambda.js
+++ APIwithSNS/APIwithSNS-lambda.js
@@ -1,7 +1,21 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
     console.log("Lambda is called");
+    
+    sns.publish({
+        Message: 'This is a test',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:480964559519:hiru_topic'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
 
 }
